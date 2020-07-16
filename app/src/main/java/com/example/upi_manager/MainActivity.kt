849c840c08ca7b90332c37522c2536ba8e29e73f
ritvij14 <ritvij2001@gmail.com>
2020-07-16 09:41:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/upi_manager/MainActivity.kt
@@ -0,0 +1,11 @@
+ package com.example.upi_manager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
